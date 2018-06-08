33475475cb196e1fae853fb670edceaad7ef7410
Pasindu G <madurange.gwp@gmail.com>
2018-06-08 19:17:29 -0400

    Deleting the default code and initialize the first approach

---
+++ src/FreeFoodFinder.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+
+import {styles} from '../src/styles/styles.js'
+
+class FreeFoodFinder extends Component{
+    render(){
+        return(
+
+            <View style={styles.mainContainer}>
+                <Text> First Code of FreeFoodFinder! Done</Text>
+
+            </View>
+
+
+
+
+        );
+    }
+}
+
+
+
+export default FreeFoodFinder;
---
+++ src/styles/styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const styles=StyleSheet.create({
+    mainContainer:{
+        flex:1,
+        width:100+"%",
+        height:100+"%",
+        justifyContent:"center",
+        alignItems:"center"
+    }
+});
